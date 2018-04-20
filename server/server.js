1cd992a95f8101a7928a486d7812ca829be372e2
Perttu Auramo <perttu.auramo@reaktor.com>
2018-04-20 14:56:01 +0300

    reformat: no semicolons

--- server/server.js
+++ server/server.js
@@ -1,28 +1,27 @@
-require("dotenv").config();
-const path = require("path");
-const headerMiddleware = require("./headerMiddleware");
+require('dotenv').config()
+const path = require('path')
+const headerMiddleware = require('./headerMiddleware')
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const compression = require("compression");
+const express = require('express')
+const bodyParser = require('body-parser')
+const compression = require('compression')
 
-const app = express();
+const app = express()
 
-headerMiddleware.init(app);
+headerMiddleware.init(app)
 
-app.use(compression({ threshold: 512 }));
+app.use(compression({ threshold: 512 }))
 
-const clientAppHtml = (req, res) =>
-  res.sendFile(path.resolve(`${__dirname}/../dist/index.html`));
-app.use("/view1*", clientAppHtml);
-app.use("/view2*", clientAppHtml);
-app.use("/", express.static(`${__dirname}/../dist`));
+const clientAppHtml = (req, res) => res.sendFile(path.resolve(`${__dirname}/../dist/index.html`))
+app.use('/view1*', clientAppHtml)
+app.use('/view2*', clientAppHtml)
+app.use('/', express.static(`${__dirname}/../dist`))
 
-app.use("/img/", express.static(`${__dirname}/../web-resources/img`));
-app.use("/css/", express.static(`${__dirname}/../web-resources/css`));
-app.use(bodyParser.json({ limit: "5000kb" }));
+app.use('/img/', express.static(`${__dirname}/../web-resources/img`))
+app.use('/css/', express.static(`${__dirname}/../web-resources/css`))
+app.use(bodyParser.json({ limit: '5000kb' }))
 
-const httpServerPort = process.env.PORT || "8080";
+const httpServerPort = process.env.PORT || '8080'
 app.listen(httpServerPort, () => {
-  console.log("server listening on port", httpServerPort);
-});
+  console.log('server listening on port', httpServerPort)
+})
